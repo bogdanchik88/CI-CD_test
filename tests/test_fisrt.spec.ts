a61346dd55174a027b5a0b3e24c27eec0fb34357
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-05-20 21:10:19 +0300

    first test

---
+++ tests/test_fisrt.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test'
+
+test('first test', async ({page}) => {
+    await page.goto('https://example.com/')
+
+    await expect(page).toHaveURL(/example/)
+})
