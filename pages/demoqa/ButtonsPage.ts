6c79fb0e8dbff11a48d09dfc08824d2f9a641b81
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-05-22 03:34:42 +0300

    Created POM of buttons page

---
+++ pages/demoqa/ButtonsPage.ts
@@ -0,0 +1,39 @@
+import { Page, Locator, expect } from "@playwright/test";
+import { BasePage } from "../BasePage";
+
+export class ButtonPage extends BasePage{
+
+    private readonly dbClickButton: Locator
+    private readonly rightClickButton: Locator
+    private readonly normalButton: Locator
+    
+    private readonly dbClickMessage: Locator
+    private readonly rightClickMessage: Locator
+    private readonly normalClickMessage: Locator
+
+    constructor(page: Page){
+        super(page)
+
+        this.dbClickButton = page.locator("#doubleClickBtn")
+        this.rightClickButton = page.locator('#rightClickBtn')
+        this.normalButton = page.getByText('Click Me')
+
+        this.dbClickMessage = page.locator('#doubleClickMessage')
+        this.rightClickMessage = page.locator('#rightClickMessage')
+        this.normalClickMessage = page.locator('#dynamicClickMessage')
+    }
+
+    async goto(){
+        await this.page.goto('/buttons')
+    }
+
+    async pressButtons(){
+        await this.dbClickButton.dblclick()
+        await this.rightClickButton.click({ button: 'right' })
+        await this.normalButton.click()
+        
+        await expect(this.dbClickMessage).toBeVisible()
+        await expect(this.rightClickMessage).toBeVisible()
+        await expect(this.normalClickMessage).toBeVisible()
+    }
+}
