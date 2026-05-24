b84314dfe304108343fac65c6b24ad22ffee889d
Bogdanchik88 <Bogdanchebotarev@gmail.com>
2026-05-24 10:24:18 +0300

    refactoring helpers functions

--- helpers/textBoxesData.ts
+++ helpers/textBoxesData.ts
@@ -2,10 +2,10 @@ export const randomString = (length: number = 8) => {
     return Math.random().toString(36).substring(2, length + 2)
 }
 
-export const randomEmail = (length: number = 8) => {
+export const randomEmail = () => {
     return `${randomString()}@email.com`
 }
 
-export const wrongEmail = (length: number = 8) => {
+export const wrongEmail = () => {
     return `${randomString()}wrong`
 }
